03a3abe56099f26df81bbc635a64531e04daec0f
kashifali123456 <kashifali123456>
2022-01-13 12:45:27 +0500

    use stylejs

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,12 +3,13 @@ import { Grid } from "@material-ui/core";
 import logo from "../../Assets/Images/logo.png";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
-import "./style.css";
+// import "./style.css";
 import { useMediaQuery } from "@material-ui/core";
 import Menu from "@material-ui/icons/Menu";
 import { IconButton } from "@material-ui/core";
 import DrawersButton from "../../Drawers";
 import EcoIcon from "@material-ui/icons/Eco";
+import useStyles from "./style";
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   const hanleDrawerOpen = () => {
@@ -18,17 +19,25 @@ const Navbar = () => {
     setOpen(false);
   };
   const match = useMediaQuery("(max-width: 960px)");
+  const classes = useStyles();
   return (
-    <Grid container className="navbar">
-      <Grid item container className="navbarimg" justify="center" xs={6} md={3}>
-        <img className="navimg" src={logo} alt="" />
-        <h2>
-          SOLTHZ <br /> <span>Aka Datusing</span>{" "}
+    <Grid container className={classes.navbar}>
+      <Grid
+        item
+        container
+        className={classes.navbarimg}
+        justify="center"
+        xs={6}
+        md={3}
+      >
+        <img className={classes.navimg} src={logo} alt="" />
+        <h2 className={classes.navimgh2}>
+          SOLTHZ <br /> <span className={classes.span}>Aka Datusing</span>{" "}
         </h2>
       </Grid>
       <Grid item container xs={1} md={2}></Grid>
       <Grid
-        className="navbar-link"
+        className={classes.navbarlink}
         justifyContent="center"
         alignItems="center"
         item
@@ -48,27 +57,31 @@ const Navbar = () => {
           </IconButton>
         ) : (
           <>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               Home
             </Link>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               Mint
             </Link>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               Stats
             </Link>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               Roadmap
             </Link>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               Team
             </Link>
-            <Link id="link" to="home">
+            <Link className={classes.link} to="home">
               FAQ
             </Link>
 
             <div className="ctw">
-              <Button id="CtW" variant="contained" color="secondary">
+              <Button
+                className={classes.CtW}
+                variant="contained"
+                color="secondary"
+              >
                 Connect to Wallet
               </Button>
             </div>
--- src/Components/Navbar/style.js
+++ src/Components/Navbar/style.js
@@ -1,12 +1,74 @@
-// import { makeStyles } from "@material-ui/core/styles";
-// const useStyles = makeStyles((theme) => ({
-//   navbarimg1: {
-//     Width: "100%",
-//     Height: "75px",
-//   },
-//   navimg: {
-//     Width: "100%",
-//     Height: "75px",
-//   },
-// }));
-// export default useStyles;
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  navbarimg: {
+    direction: "row",
+    alignItems: "center",
+    color: "#fff",
+    textAlign: "center",
+  },
+
+  navbar: {
+    background:
+      "linear-gradient( 111.31deg, #090a0b 7.84%, rgba(31, 7, 82, 0) 94.81% ), #41153f",
+    width: "100%",
+    opacity: "0.9",
+    padding: "5px 0 5px",
+    height: " 70px",
+    alignItems: "center",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    boxShadow: "0 2px 15px #891f85",
+    transition: "all 0.5s",
+    zIndex: "997",
+  },
+  navimg: {
+    width: "50px",
+    padding: "2px",
+    margin: "2px",
+    height: "40px",
+  },
+  navbarimgh2: {
+    fontSize: "24px",
+    fontWeight: "800",
+    textShadow: "0 0 7px orchid",
+  },
+  span: {
+    display: "block",
+    lineHeight: "1",
+    fontWeight: "300",
+    letterSpacing: "4px",
+    fontSize: "12px",
+  },
+
+  link: {
+    color: "#fff",
+    textShadow: "0 0 7px rgb(218 112 214 / 50%)",
+    textDecoration: "none",
+    fontSize: "16px",
+    padding: "10px",
+    transition: "all 0.5s ease-in-out",
+    marginLeft: "5px",
+  },
+
+  CtW: {
+    borderRadius: "20px",
+    backgroundColor: "transparent !important",
+    border: "1px solid orchid",
+    color: "orchid",
+    fontSize: "12px",
+    textTransform: "capitalize",
+    textShadow: "0 0 7px rgb(218 112 214 / 50%) !important",
+    boxShadow: "0 0 15px rgb(218 112 214 / 30%) !important",
+    marginLeft: "30px",
+    letterSpacing: "1px",
+    marginTop: "3px",
+    height: " 40px",
+    paddingLeft: "12px",
+    paddingRight: "12px",
+    cursor: "pointer",
+    fontWeight: "500",
+  },
+}));
+export default useStyles;
